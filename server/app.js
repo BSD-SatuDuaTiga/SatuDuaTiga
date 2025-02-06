2f9c9e0965bf815ced61a8eeedcda6bf7858c012
robbyxsuganda <robbyxsuganda@gmail.com>
2025-02-06 13:32:49 +0700

    fix port server

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ if (process.env.NODE_ENV !== "production") {
 
 const express = require("express");
 const app = express();
-const port = proccess.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 
